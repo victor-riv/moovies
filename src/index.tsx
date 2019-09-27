78547312aabddd51a8cc5e2fb9de52562ba9f188
Victor Rivera <verm2405@gmail.com>
2019-09-27 08:48:25 -0400

    progress on login page

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.scss";
 import { LoginPage } from "./pages/login";
 import * as serviceWorker from "./serviceWorker";
 
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,20 @@
-import React from "react";
-import "../styles/login.css";
+import React, { useReducer } from "react";
+import "../styles/login.scss";
 
 const LoginPage = () => {
   return (
     <div className="login-page">
       <div className="login-card">
         <div className="login-left" />
-        <div className="login-right" />
+        <div className="login-right">
+          <form className="login-form-container">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+            <button> Submit </button>
+          </form>
+        </div>
       </div>
     </div>
   );
