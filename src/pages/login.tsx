58aee0a355cf6ac7c2efa1ed179c86349a36609d
Victor Rivera <verm2405@gmail.com>
2019-09-27 07:43:19 -0400

    initializing project

---
+++ src/pages/login.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/login.css";
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <div className="login-card">
+        <div className="login-left" />
+        <div className="login-right" />
+      </div>
+    </div>
+  );
+};
+
+export { LoginPage };
